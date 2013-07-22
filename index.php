f03be27263eabb24f81498ef192bd21488881b52
Adrian <adrian.delarosab@gmail.com>
2013-07-22 20:51:14 +0200

    Alpha version 0.1

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <!-- META -->
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="author" content="Adrian de la Rosa Bretin" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="robots" content="all" />
+
+        <!-- TITLE -->
+        <title>Adri&aacute;n de la Rosa Bret&iacute;n</title>
+
+        <!-- BASE HREF -->
+        <base href="http://localhost/adrian.delarosab.me/" />
+
+        <!-- CSS -->
+        <link href="css/normalize.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/default.css" rel="stylesheet" type="text/css" />
+
+        <!-- FAVICON -->
+        <link rel="shortcut icon" href="images/favicon.ico" />
+    </head>
+    <body>
+        <div class="line"></div>
+        <header>
+            <img alt="Adri&aacute;n de la Rosa" id="logotype" src="images/logotype.png" />
+            <h2 id="slogan">Bienvenido a mi p&aacute;gina web personal!</h2>
+        </header>
+        <section>
+            <section>
+                <article>
+                    <div class="line"></div>
+                    <h1>About me</h1>
+                    <p>
+                        ¡Hola! Soy Adri&aacute;n. Soy <del>casi</del> Ingeniero
+                        Inform&aacute;tico por la Universidad Rey Juan Carlos. Desde
+                        el a&ntilde;o 2009 he trabajo como dise&ntilde;ador web,
+                        realizando trabajos para empresas del sector y el gobierno
+                        de Espa&ntilde;a.
+                    </p>
+                    <p>
+                        En 2005 comenc&eacute; mis andaduras en el desarrollo web
+                        con una p&aacute;niga web de cr&iacute;tica de videojuegos.
+                        Posteriormente esta curiosidad me empuj&oacute; a estudiar
+                        PHP.
+                    </p>
+                    <p>
+                        Por fin, en 2009 consegu&iacute; mi primer puesto como
+                        programador web en <a href="http://www.virtualname.es">
+                            Estrategias Website S.L.</a>. Desarroll&eacute; una
+                        intranet para la gesti&oacute;n de clientes y dominios
+                        (conectada a las APIs de ESNIC e ICANN).
+                    </p>
+                    <p>
+                        Desde entonces he desarrollado p&aacute;ginas de todo tipo:
+                        desde p&aacute;ginas promocionales a aplicaciones tipo
+                        Wordpress.
+                    </p>
+                </article>
+                <article>
+                    <div class="line"></div>
+                    <h1>Portfolio</h1>
+                </article>
+            </section>
+            <aside>
+                <article>
+                    <div class="line"></div>
+                    <h1>Contact</h1>
+                    Hello world!
+                </article>
+                <article>
+                    <div class="line"></div>
+                    <h1>Twitter</h1>
+                    <a class="twitter-timeline" href="https://twitter.com/adriandelarosab" data-widget-id="357662886191050752">Tweets by @adriandelarosab</a>
+                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                </article>
+            </aside>
+        </section>
+    </body>
+</html>
